7deecb13ef51dfaec4af287831f62aadea96ff4e
Michal Dobrzycki <dobrzycki@gmail.com>
2023-05-09 10:18:19 +0200

    Initial commit

---
+++ src/main/java/services/Calculator.java
@@ -0,0 +1,18 @@
+package services;
+
+public class Calculator {
+    public int add(int a, int b) {
+        return a+b;
+    }
+    public int subtract(int a, int b) {
+        return a-b;
+    }
+
+    public int mul(int a, int b) {
+        return a*b;
+    }
+
+    public int div(int a, int b) {
+        return a/b;
+    }
+}
